1495d32354132f8d8a0be085953e0f37b9213794
Isaac Iniongun <iniongunisaac@gmail.com>
2020-01-29 18:26:33 +0100

    Initial Commit

---
+++ Posts App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts App
+//
+//  Created by Isaac Iniongun on 29/01/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
